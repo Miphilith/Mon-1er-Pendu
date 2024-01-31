754e56c0a05e828dfa3d61a6ce4c0081ca1c5f08
Miphilith <loreliagalopin@gmail.com>
2024-01-31 20:47:34 +0100

    addStringLength

--- Assets/Script/LetterManager.cs
+++ Assets/Script/LetterManager.cs
@@ -8,13 +8,14 @@ public class Manager : MonoBehaviour
 
     private string hideWord;
     private int random;
+    private int counterLetter;
 
     void Start()
     {
-        
-        random = Random.Range(0, 4); //Prend un nombre antier aléatoire
+        random = Random.Range(0, words.Length); //Prend un nombre antier aléatoire en fonction de la longueur du tableau
         hideWord = words[random]; //Prend le mot en fonction du nombre aléatoire
+        counterLetter = hideWord.Length; //Compte la longueur du mot sélectionner
 
-        Debug.Log(hideWord);
+        Debug.Log(hideWord + " " + counterLetter);
     }
 }

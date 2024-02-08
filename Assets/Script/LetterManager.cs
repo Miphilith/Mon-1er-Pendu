03d6ca3b812d390cc404f5bd4f8d6ca59098e5d0
Miphilith <loreliagalopin@gmail.com>
2024-02-08 15:53:07 +0100

    check word

--- Assets/Script/LetterManager.cs
+++ Assets/Script/LetterManager.cs
@@ -1,21 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEditor.Search;
 
 public class Manager : MonoBehaviour
 {
     private string[] words = new string[] { "abundant", "eccentric", "glimmer", "whimsical" }; //Prédétermine les mots
 
     private string hideWord;
+    private string showWord;
     private int random;
-    private int counterLetter;
+
+    [SerializeField]
+    private Image pendu_image;
+    [SerializeField]
+    private Sprite[] penduSprites;
+    [SerializeField]
+    private TextMeshProUGUI wordDisplay;
+    private VirtualKeyboard keyboard;
 
     void Start()
     {
+        keyboard = GetComponent<VirtualKeyboard>();
         random = Random.Range(0, words.Length); //Prend un nombre antier aléatoire en fonction de la longueur du tableau
         hideWord = words[random]; //Prend le mot en fonction du nombre aléatoire
-        counterLetter = hideWord.Length; //Compte la longueur du mot sélectionner
+        for (int i = 0; i < hideWord.Length; i++)
+        {
+            wordDisplay.text += '_';
+        }
+        showWord = wordDisplay.text.ToString();
+        Debug.Log(hideWord);
+    }
 
-        Debug.Log(hideWord + " " + counterLetter);
+    public void CheckLetter()
+    {
+        if (hideWord.ToUpper().Contains(keyboard.letter))
+        {
+            for (int i = 0; i < hideWord.Length; i++)
+            {
+                if (hideWord[i].Equals(keyboard.letter))
+                {
+                    showWord[i] = keyboard.letter;
+                }
+            }
+            Debug.Log(showWord + ' ' + true);
+
+        }
+        else
+        {
+            Debug.Log(false);
+        }
     }
 }
